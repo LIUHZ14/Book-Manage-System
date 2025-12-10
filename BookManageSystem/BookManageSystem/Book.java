6fe74486ecbfc9e8300c22eb839cafc714bfd3ac
LIUHZ14 <2875781923@qq.com>
2025-12-10 11:30:48 +0800

    add comments
    Added simple English comments

---
+++ BookManageSystem/BookManageSystem/Book.java
@@ -0,0 +1,6 @@
+package BookManageSystem;
+public class Book {// Defines a public class named Book to represent a book in the library system
+    String bookname;// Instance variable to store the name of the book
+    String bookauthor;// Instance variable to store the author of the book
+    int bookprice;// Instance variable to store the price of the book
+}
